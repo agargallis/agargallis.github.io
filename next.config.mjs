d59eb588f0f60b105a9a4c0a32a5ee56e5b27cbd
agargallis <antonisgargallis@gmail.com>
2025-04-24 17:41:07 +0300

    adding details

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,9 @@ const nextConfig = {
     domains: ['placeholder.com'],
     unoptimized: true,
   },
+  output: 'export', // 🔥 Χρειάζεται για να παραχθεί ο φάκελος `out/`
+  basePath: '', // Εάν το έργο σου είναι στο root του GitHub Pages, άφησέ το κενό
+  assetPrefix: '', // Για σωστά static αρχεία, άφησέ το κενό αν το project είναι στον root
 };
 
 export default nextConfig;
