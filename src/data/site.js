a0e1ff4a048bb3af263314493405ce966126db0b
agargallis <antonisgargallis@gmail.com>
2026-02-21 21:57:57 +0200

    Fixed

--- src/data/site.js
+++ src/data/site.js
@@ -265,25 +265,17 @@ export const site = {
     work: [
       {
         title: "Referee Specialist",
-<<<<<<< HEAD
-        period: "FEB 2026 - TILL NOW",
-=======
-        period: "MAR 2025 - TILL NOW",
->>>>>>> fb2740a9c450e8ee621193ca3515b255e80e8eb7
+        period: "MAR 2026 - TILL NOW",
         description: "Basketaki The League",
       },
       {
         title: "Statistics Specialist",
-<<<<<<< HEAD
-        period: "JAN 2025 - JAN 2026",
-=======
-        period: "JAN 2024 - FEB 2025",
->>>>>>> fb2740a9c450e8ee621193ca3515b255e80e8eb7
+        period: "JAN 2025 - FEB 2026",
         description: "Basketaki The League",
       },
       {
         title: "Teaching Assistant of Blind Student",
-        period: "JAN 2025 - JUNE 2025",
+        period: "JAN 2025 - JUN 2025",
         description: "National & Kapodistrian University of Athens",
         image: {
           src: "/images/18.png",
