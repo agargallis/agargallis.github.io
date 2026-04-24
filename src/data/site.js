6d358106d2052393cbda5b97421a1fc63e1434ea
agargallis <antonisgargallis@gmail.com>
2026-04-24 16:33:48 +0300

    fix contact form

--- src/data/site.js
+++ src/data/site.js
@@ -150,7 +150,7 @@ export const site = {
       },
       {
         title: "Menu & QR Automation Designing",
-        href: "https://www.papermark.com/view/cmoaho7ur001uji04cu7ps7df",
+        href: "https://www.papermark.com/view/cmocy7tdd002ol204d6ttqqtu",
         icon: "https://i.imgur.com/KMNGx7t.png",
       },
       {
