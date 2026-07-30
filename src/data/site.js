d3a85f942cfd191d6c874e17f1c521da0cbca0d1
agargallis <antonisgargallis@gmail.com>
2026-07-30 15:45:28 +0300

    done

--- src/data/site.js
+++ src/data/site.js
@@ -63,7 +63,7 @@ export const site = {
   about: {
     title: "Few things about me",
     intro:
-      "I was born in October of 2006 in Athens. I successfully completed my high school education at the 9th General Lyceum of Peristeri in June of 2024, and I am currently pursuing my studies in the Department of Informatics and Telecommunications at the National and Kapodistrian University of Athens. I am working also as a Referee Specialist for Basketaki The League, and I am the founder & total manager of UBD, a startup focused on providing comprehensive digital solutions for businesses.",
+      "I was born in October of 2006 in Athens. I successfully completed my high school education at the 9th General Lyceum of Peristeri in June of 2024, and I am currently pursuing my studies in the Department of Informatics and Telecommunications at the National and Kapodistrian University of Athens. I am the founder & total manager of UBD, a startup focused on providing comprehensive digital solutions for businesses.",
     highlights: [
       "Focus: Web Solutions & Digital Growth",
       "Driven by: Innovation, Speed, Quality",
@@ -340,7 +340,7 @@ export const site = {
       },*/
       {
         title: "Statistics Specialist",
-        period: "JAN 2025 - NOW",
+        period: "JAN 2025 - JUN 2026",
         description: "Basketaki The League",
       },
       {
